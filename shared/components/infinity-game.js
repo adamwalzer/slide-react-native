c325ef0305fb30d1fcb24340367b74e67f278adf
Adam Walzer <awalzer@gottanom.com>
2016-01-17 09:40:56 -0500

    infinity game

---
+++ shared/components/infinity-game.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var GameTemplate = require('./helpers/game-template.js');
+var loop = require('./helpers/loop.js');
+
+var opts = {
+  title: "infinity",
+  values: [1],
+  makeSpaces: function(b,values) {
+    var spaces = [];
+    loop.each(b, function(c,i) {
+      loop.each(c, function(d,j) {
+        if(d) {
+          values.push(d.val());
+        } else {
+          spaces.push({x:i,y:j});
+        }
+      });
+    });
+    return spaces;
+  },
+  newZ: function(values) {
+    return Math.max(Math.min.apply(null,values)-Math.floor(Math.random()*10/9),1);
+  },
+  combineVal: function(v) {
+    return v+1;
+  }
+};
+
+var InfinityGame = GameTemplate(opts);
+
+module.exports = InfinityGame;
